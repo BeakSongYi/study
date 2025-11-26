c98e4865979036704b2515c6e5f2d636c92f0a5e
songyi <ssejong00310@gmail.com>
2025-11-26 22:36:58 +0900

    배열 관련 코딩테스트 문제 및 프로젝트 신규생성

---
+++ codingtest_java/solution/coding_test/src/com/javaTest/algo01.java
@@ -0,0 +1,22 @@
+package com.javaTest;
+
+import java.util.Arrays;
+
+/* 01.배열 정렬하기 */
+public class algo01 {
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, -5, 2, 4, 3})));
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+        System.out.println(Arrays.toString(solution(new int[]{6, 1, 7})));
+    }
+
+    //O(NlogN)
+    private static int[] solution(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+
+
+}
